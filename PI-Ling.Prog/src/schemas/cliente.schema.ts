b4721bf9297ed62ed1432e4c1e10a65ea5c6d7da
Lucas Neves Fregnani - LINUX <lucasnfregnani@gmail.com>
2026-05-26 19:50:19 -0300

    .

--- PI-Ling.Prog/src/schemas/cliente.schema.ts
+++ PI-Ling.Prog/src/schemas/cliente.schema.ts
@@ -8,12 +8,11 @@ import { z } from "zod";
 
 export const ClienteSchema = z.object({
   nome: z.string().min(2, "Nome deve ter ao menos 2 caracteres"),
-  
-    telefone: z.string()
-    .nonempty("Telefone é obrigatório")
-    .min(10, "Telefone deve ter no mínimo 12 caracteres (incluindo DDD)")
-    .max(12, "Telefone deve ter no máximo 15 caracteres (incluindo DDD)")
-    .regex(/^\d+$/, "Telefone deve conter apenas números"),
+  telefone: z.string()
+  .nonempty("Telefone é obrigatório")
+  .min(10, "Telefone deve ter no mínimo 12 caracteres (incluindo DDD)")
+  .max(12, "Telefone deve ter no máximo 15 caracteres (incluindo DDD)")
+  .regex(/^\d+$/, "Telefone deve conter apenas números"),
   email: z.string().email("E-mail inválido").optional(),
   endereco: z.string().optional(),
 });

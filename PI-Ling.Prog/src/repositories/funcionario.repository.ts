5015c896ac2f06cc390830366f59aa4847221801
Murilo Rezende Vieira <140859440+MuhVieira@users.noreply.github.com>
2026-05-31 19:03:17 -0300

    Adiciona busca por email nas operações do repository e atualiza a query de atualização para incluir o campo email

--- PI-Ling.Prog/src/repositories/funcionario.repository.ts
+++ PI-Ling.Prog/src/repositories/funcionario.repository.ts
@@ -5,9 +5,11 @@ import { error } from "node:console";
 
 // Murilo aqui! Alterei as querys e deixei elas separadas para que elas sejam preparadas apenas uma vez quando o server iniciar
 // Alterei também as operações do repository com o return de statements
+// Adicionado busca por emial nas querys e operações
 const statements = {
   listarTodos: db.prepare("SELECT * FROM Funcionario WHERE ativo = 1"),
   buscarPorId: db.prepare("SELECT * FROM Funcionario WHERE id_funcionario = ?"),
+  buscarPorEmail: db.prepare("SELECT * FROM Funcionario WHERE email = ?"),
   criar: db.prepare(`
     INSERT INTO Funcionario (nome, telefone, cargo, ativo, data_admissao)
     VALUES (@nome, @telefone, @cargo, @ativo, @data_admissao)`),
@@ -16,6 +18,7 @@ const statements = {
       UPDATE Funcionario SET
       nome = COALESCE(@nome, nome),
       telefone = COALESCE(@telefone, telefone),
+      email = COALESCE(@email, email),
       cargo = COALESCE(@cargo, cargo),
       ativo = COALESCE(@ativo, ativo)
       WHERE id_funcionario = @id)
@@ -32,6 +35,10 @@ export const funcionarioRepository = {
   buscarPorId: (id: number): Funcionario | undefined => {
     return statements.buscarPorId.get(id) as Funcionario | undefined;
   },
+
+  buscarPorEmail: (email: string): Funcionario | undefined => {
+  return statements.buscarPorEmail.get(email) as Funcionario | undefined;
+},
 
   criar: (dados: Omit<Funcionario, "id_funcionario">): Funcionario => {
     const resultado = statements.criar.run(dados);
